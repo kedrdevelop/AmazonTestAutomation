2bdbef4605c8716ad9c8615e201d1bcd266adcdb
Viacheslav Kedrov <kedrov.develop@gmail.com>
2026-04-21 12:28:39 +0200

    1. Test 1.2 Done

--- src/test/java/Task1AmazonTests.java
+++ src/test/java/Task1AmazonTests.java
@@ -2,16 +2,21 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Task1AmazonTests {
+    // ANSI color codes for console output
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_RESET = "\u001B[0m";
+
     private WebDriver driver;
     private AmazonHomePage amazonHomePage;
     private WebDriverWait wait;
@@ -29,28 +34,39 @@ public class Task1AmazonTests {
     @Test
     public void testAmazonStartPageLoads() {
         try {
-            driver.get("https://www.amazon.de/");
-
-            // Wait, until the title is 'Amazon'
-            wait.until(ExpectedConditions.titleContains("Amazon"));
-
-            String pageTitle = driver.getTitle();
-            System.out.println("DEBUG ACTUAL TITLE: '" + pageTitle + "'");
-
-            Assertions.assertTrue(pageTitle.contains("Amazon"),
-                    "The loaded page title does not contain 'Amazon'");
-
-            System.out.println("Die Amazon Startseite wird erfolgreich aufgerufen");
+            test11();
+            test12();
         } catch (Exception e) {
             System.err.println("ERROR: " + e.getMessage());
             Assertions.fail("Test FAILED due:" + e.getMessage());
         }
     }
 
+    private void test11() {
+        driver.get("https://www.amazon.de/");
 
-    @Test
-    public void testAmazonSneakersSearch() {
+        // Wait, until the title is 'Amazon'
+        wait.until(ExpectedConditions.titleContains("Amazon"));
 
+        String pageTitle = driver.getTitle();
+
+        Assertions.assertTrue(pageTitle.contains("Amazon"),
+                "The loaded page title does not contain 'Amazon'");
+        printSuccess("Die Amazon Startseite wird erfolgreich aufgerufen");
+    }
+
+    private void test12() {
+        amazonHomePage.searchForProduct("Adidas Schuhe");
+        By result = By.cssSelector("div[data-component-type='s-search-result']");
+        wait.until(ExpectedConditions.presenceOfElementLocated(result));
+
+        List<WebElement> searchResults = driver.findElements(result);
+
+        Assertions.assertTrue(searchResults.size() >= 5,
+                "Expected 5, but found: " + searchResults.size());
+
+        printSuccess("Es werden mindestens 5 Schuhe angezeigt (Gefunden: "
+                + searchResults.size() + ")");
     }
 
     @AfterEach
@@ -60,4 +76,7 @@ public class Task1AmazonTests {
         }
     }
 
+    private void printSuccess(String message) {
+        System.out.println(ANSI_GREEN + message + ANSI_RESET);
+    }
 }
